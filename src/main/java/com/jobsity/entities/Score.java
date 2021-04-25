bdc3bf4e5f4f9f2333cb20e5cbc927163913185a
David Santos <insa1988@gmail.com>
2021-04-24 23:35:38 -0500

    add new fields Score.java

--- src/main/java/com/jobsity/entities/Score.java
+++ src/main/java/com/jobsity/entities/Score.java
@@ -6,15 +6,35 @@ public class Score {
     private int pinsValue2;
     private boolean isStrike;
     private boolean isSpare;
+    private boolean isFoul;
+    private boolean isOpen;
 
     public Score() {
     }
 
-    public Score(int pinsValue1, int pinsValue2, boolean isStrike, boolean isSpare) {
+    public Score(int pinsValue1, int pinsValue2, boolean isStrike, boolean isSpare, boolean isFoul, boolean isOpen) {
         this.pinsValue1 = pinsValue1;
         this.pinsValue2 = pinsValue2;
         this.isStrike = isStrike;
         this.isSpare = isSpare;
+        this.isFoul = isFoul;
+        this.isOpen = isOpen;
+    }
+
+    public boolean isFoul() {
+        return isFoul;
+    }
+
+    public void setFoul(boolean foul) {
+        isFoul = foul;
+    }
+
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+    public void setOpen(boolean open) {
+        isOpen = open;
     }
 
     public int getPinsValue1() {
